3805ca2333ba14eda56e20ede45e1c74abc60651
vic230387 <cuaco37@hotmail.com>
2013-05-30 04:31:25 -0500

    Correción Ventas Final
    Orden de imágenes en el paquete correspondiente y delete de imágenes
    repetidas

---
+++ src/Ventas/Resultset.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ventas;
+
+/**
+ *
+ * @author vic1
+ */
+class Resultset {
+
+    String getString(String fecha) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/Ventas/conectar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ventas;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author vic1
+ */
+class conectar {
+
+    Connection conexion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
