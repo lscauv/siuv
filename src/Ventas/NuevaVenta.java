0b7b2619de7069462b6c82283b3e9a55c66dfa98
Santiago Ruiz <redman_rhcp@hotmail.com>
2013-05-30 20:28:17 -0500

    Nueva venta corregida
    agregada la fecha

--- src/Ventas/NuevaVenta.java
+++ src/Ventas/NuevaVenta.java
@@ -37,7 +37,7 @@ public class NuevaVenta extends javax.swing.JFrame {
         this.psExecute = null;
         initComponents();
         data.setUser("root");
-        data.setPassword("");
+        data.setPassword("1234");
         data.setDatabaseName("siuv");
         data.setServerName("127.0.0.1");
         lFecha.setText(fechaActual());
@@ -100,7 +100,7 @@ public class NuevaVenta extends javax.swing.JFrame {
 
     public static String fechaActual() {
         Date fecha = new Date();
-        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/YYYY");
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("YYYY-MM-dd");
         return formatoFecha.format(fecha);
     }
 
@@ -595,10 +595,10 @@ public class NuevaVenta extends javax.swing.JFrame {
         nFactura.addAncestorListener(new javax.swing.event.AncestorListener() {
             public void ancestorMoved(javax.swing.event.AncestorEvent evt) {
             }
+            public void ancestorRemoved(javax.swing.event.AncestorEvent evt) {
+            }
             public void ancestorAdded(javax.swing.event.AncestorEvent evt) {
                 nFacturaAncestorAdded(evt);
-            }
-            public void ancestorRemoved(javax.swing.event.AncestorEvent evt) {
             }
         });
 
@@ -609,12 +609,12 @@ public class NuevaVenta extends javax.swing.JFrame {
 
         comboProducto.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "ID", "Nombre" }));
         comboProducto.addPopupMenuListener(new javax.swing.event.PopupMenuListener() {
-            public void popupMenuCanceled(javax.swing.event.PopupMenuEvent evt) {
-            }
             public void popupMenuWillBecomeInvisible(javax.swing.event.PopupMenuEvent evt) {
                 comboProductoPopupMenuWillBecomeInvisible(evt);
             }
             public void popupMenuWillBecomeVisible(javax.swing.event.PopupMenuEvent evt) {
+            }
+            public void popupMenuCanceled(javax.swing.event.PopupMenuEvent evt) {
             }
         });
         comboProducto.addActionListener(new java.awt.event.ActionListener() {
@@ -892,6 +892,7 @@ public class NuevaVenta extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
@@ -1172,8 +1173,8 @@ public class NuevaVenta extends javax.swing.JFrame {
                 int eleccion = JOptionPane.showOptionDialog(this, "¿Desea confirmar la Venta?", nombre, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, "Aceptar");
                 if(eleccion==JOptionPane.YES_OPTION)
                 {
-                    siuv.MysqlExecute ventas = new siuv.MysqlExecute();
-        ventas.Insertar("ventas", "'" + nFactura.getText() + "','" + txtCantidad.getText() + "','" + SubTotal.getText() + "','" + IVA.getText() + "','" + Total.getText() + "','" + TipoPago.getText() + "','" + txtBanco.getText() + "','" + lID.getText() + "','" + lFecha.getText() + "','" + txtRFC.getText() + "'");
+                    siuv.MysqlExecute venta = new siuv.MysqlExecute();
+        venta.Insertar("ventas", "'" + nFactura.getText() + "','" + txtCantidad.getText() + "','" + SubTotal.getText() + "','" + IVA.getText() + "','" + Total.getText() + "','" + TipoPago.getText() + "','" + txtBanco.getText() + "','" + lID.getText() + "','" + txtRFC.getText() + "','" + lFecha.getText() +"'");
                 }
     }//GEN-LAST:event_btnVentaActionPerformed
 
